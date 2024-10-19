8388230ffc04e09bcaecf3c7d05169f6916bdda3
Karla Nguyen <karlaa09861@gmail.com>
2024-10-18 22:02:11 -0700

    sidebar

--- frontend/app/lib/utils.ts
+++ frontend/app/lib/utils.ts
@@ -1,6 +1,13 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+<<<<<<< HEAD
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+=======
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+>>>>>>> 423c473 (sidebar)
