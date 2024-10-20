8bec7788f43fcaef830edb90f7c73fe838138cdd
Richard Yip <rdyip892@gmail.com>
2024-10-20 03:46:31 -0700

    Write routes for outfits table

--- backend/helper.py
+++ backend/helper.py
@@ -1,23 +1,27 @@
-from dotenv import load_dotenv
+import hashlib
+import json
+import os
+
 import google.cloud.storage
 import singlestoredb as s2
-import hashlib
-import os
-import json
-from describe import get_clothing_JSON
-from describe import embed
+from describe import embed, get_clothing_JSON
+from dotenv import load_dotenv
 
 load_dotenv()
+
 
 def create_user(first, last, username, password, email):
     try:
         conn = s2.connect(os.getenv("SSDB_URL"))
         with conn.cursor() as cur:
             password = hashlib.sha256(password.encode()).hexdigest()
-            cur.execute("INSERT INTO users (firstName, lastName, username, pass, email) VALUES (%s, %s, %s, %s, %s)", 
-                        (first, last, username, password, email))
+            cur.execute(
+                "INSERT INTO users (firstName, lastName, username, pass, email) VALUES (%s, %s, %s, %s, %s)",
+                (first, last, username, password, email),
+            )
     except Exception as e:
         print("Error: ", e)
+
 
 def delete_user(id):
     try:
@@ -26,26 +30,33 @@ def delete_user(id):
             cur.execute("DELETE FROM users WHERE id = %s", (id))
     except Exception as e:
         print("Error: ", e)
-        
+
+
 def get_userID(email, password):
     try:
         conn = s2.connect(os.getenv("SSDB_URL"))
         with conn.cursor() as cur:
             password = hashlib.sha256(password.encode()).hexdigest()
-            cur.execute("SELECT id FROM users WHERE email = %s AND pass = %s", (email, password))
+            cur.execute(
+                "SELECT id FROM users WHERE email = %s AND pass = %s", (email, password)
+            )
             return cur.fetchall()
     except Exception as e:
         print("Error: ", e)
+
 
 def add_clothe(description, category, imgURL, userID, embedding):
     try:
         conn = s2.connect(os.getenv("SSDB_URL"))
         with conn.cursor() as cur:
-            cur.execute("INSERT INTO clothe (descriptionn, category, imgURL, userID, description_embedding) VALUES (%s, %s, %s, %s, %s)", 
-                        (description, category, imgURL, userID, embedding))
+            cur.execute(
+                "INSERT INTO clothe (descriptionn, category, imgURL, userID, description_embedding) VALUES (%s, %s, %s, %s, %s)",
+                (description, category, imgURL, userID, embedding),
+            )
     except Exception as e:
         print("Error: ", e)
-        
+
+
 def delete_clothe(id):
     try:
         conn = s2.connect(os.getenv("SSDB_URL"))
@@ -54,14 +65,18 @@ def delete_clothe(id):
     except Exception as e:
         print("Error: ", e)
 
+
 def edit_description(id, description):
     try:
         conn = s2.connect(os.getenv("SSDB_URL"))
         with conn.cursor() as cur:
-            cur.execute("UPDATE clothe SET descriptionn = %s WHERE id = %s", (description, id))
+            cur.execute(
+                "UPDATE clothe SET descriptionn = %s WHERE id = %s", (description, id)
+            )
     except Exception as e:
         print("Error: ", e)
-        
+
+
 def edit_category(id, category):
     try:
         conn = s2.connect(os.getenv("SSDB_URL"))
@@ -74,27 +89,36 @@ def edit_category(id, category):
 def upload_file(file, file_name):
     try:
         storage_client = google.cloud.storage.Client()
-        bucket = storage_client.get_bucket('bucket_of_photos')
+        bucket = storage_client.get_bucket("bucket_of_photos")
 
-        blob = bucket.blob(file_name+".png")
-        blob.upload_from_file(file, content_type=f'image/png')
-        
+        blob = bucket.blob(file_name + ".png")
+        blob.upload_from_file(file, content_type=f"image/png")
+
         # generate gemini image description
         response = get_clothing_JSON(blob.public_url)
         json_response = json.loads(response)
-        description = json_response['description'] + " tags: " + ", ".join(json_response['tags'])
+        description = (
+            json_response["description"] + " tags: " + ", ".join(json_response["tags"])
+        )
         embedding = str(embed(description)["embedding"])
-        add_clothe(description, json_response['type'], blob.public_url, 2251799813685250, embedding)
-        
+        add_clothe(
+            description,
+            json_response["type"],
+            blob.public_url,
+            2251799813685250,
+            embedding,
+        )
+
     except Exception as e:
         print("Error: ", e)
-    
+
+
 def rag_top_items(vibe, limit, clothing_type):
     try:
         conn = s2.connect(os.getenv("SSDB_URL"))
         with conn.cursor() as cur:
             search = embed(vibe)["embedding"]
-            
+
             query = f"""
             SELECT id, descriptionn, category, description_embedding <*> ('{search}':>VECTOR(768)) AS score 
             FROM clothe
@@ -102,13 +126,49 @@ def rag_top_items(vibe, limit, clothing_type):
             ORDER BY score DESC
             LIMIT {limit};
             """
-            
+
             cur.execute(query)
-            
+
             results = cur.fetchall()
             for row in results:
                 print(f"Type: {row[2]}, Description: {row[1]}, Score: {row[3]} \n")
-            
+
             return results
+    except Exception as e:
+        print("Error: ", e)
+
+
+def add_outfit(userID, top_part_url, bottom_part_url, date_added, saved):
+    try:
+        conn = s2.connect(os.getenv("SSDB_URL"))
+        with conn.cursor() as cur:
+            cur.execute(
+                "INSERT INTO outfits (userID, top_part, bottom_part, date_added, saved) VALUES (%s, %s, %s, %s, %s)",
+                (userID, top_part_url, bottom_part_url, date_added, saved),
+            )
+    except Exception as e:
+        print("Error: ", e)
+
+
+def get_outfits(userID, start_date, end_date):
+    try:
+        conn = s2.connect(os.getenv("SSDB_URL"))
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT top_part, bottom_part FROM outfits WHERE userID = %s AND %s <= date_added AND date_added <= %s",
+                (userID, start_date, end_date),
+            )
+    except Exception as e:
+        print("Error: ", e)
+
+
+def get_saved(userID):
+    try:
+        conn = s2.connect(os.getenv("SSDB_URL"))
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT top_part, bottom_part FROM outfits WHERE userID = %s AND saved = 1",
+                (userID),
+            )
     except Exception as e:
         print("Error: ", e)
